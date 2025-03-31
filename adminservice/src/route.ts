a17a5c7fef5a4b6e97f61cb8c585f6fccdf9d03a
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-03-31 23:52:34 +0530

    implement thumbnail upload and delete routes for songs and albums with admin role verification

--- adminservice/src/route.ts
+++ adminservice/src/route.ts
@@ -3,17 +3,17 @@ import uploadFile, { isAuth } from "./middleware.js";
 import {
   addAlbum,
   addSong,
-//   addThumbnail,
-//   deleteAlbum,
-//   deleteSong,
+  addThumbnail,
+  deleteAlbum,
+  deleteSong,
 } from "./controller.js";
 
 const router = express.Router();
 
 router.post("/album/new", isAuth, uploadFile, addAlbum);
 router.post("/song/new", isAuth, uploadFile, addSong);
-// router.post("/song/:id", isAuth, uploadFile, addThumbnail);
-// router.delete("/album/:id", isAuth, deleteAlbum);
-// router.delete("/song/:id", isAuth, deleteSong);
+router.post("/song/:id", isAuth, uploadFile, addThumbnail);
+router.delete("/album/:id", isAuth, deleteAlbum);
+router.delete("/song/:id", isAuth, deleteSong);
 
 export default router;

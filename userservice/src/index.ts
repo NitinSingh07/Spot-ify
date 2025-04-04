93bdc7d6173f9405a5bcbcf30afb2469c04e9f04
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-04-05 02:17:22 +0530

    add CORS middleware to user service for handling cross-origin requests

--- userservice/src/index.ts
+++ userservice/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoutes from "./route.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ const connectDb = async () => {
 };
 
 const app = express();
-
+app.use(cors());
 
 app.use(express.json());
 

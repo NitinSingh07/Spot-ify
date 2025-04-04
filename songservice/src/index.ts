db3fa040598d34a69723b14a87bf703ad70e7293
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-04-05 01:00:02 +0530

    add Redis client setup and connection handling in the song service

--- songservice/src/index.ts
+++ songservice/src/index.ts
@@ -1,8 +1,25 @@
 import express from "express";
 import doetnv from "dotenv";
 import songRoutes from "./route.js";
-
+import redis from "redis";
 doetnv.config();
+
+export const redisClient = redis.createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+redisClient
+  .connect()
+  .then(() => {
+    console.log("redis connected");
+  })
+  .catch((err) => {
+    console.log("redis not connected", err);
+  });
 
 const app = express();
 

1dc9a87060b9ebf2fdf125567ad4bb9c150491b9
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-04-12 02:38:58 +0530

    feat: implement Layout component for consistent page structure with Navbar, Sidebar, and Player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { useUserData } from "./context/UserContext";
+import Loading from "./components/Loading";
 
 const App = () => {
+  const { isAuth, loading } = useUserData();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
 };
 
